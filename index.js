31d1b369be7455d59da66715363753be68660ab1
Huy Tu <tranhuytu242000@gmail.com>
2022-06-02 12:14:51 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('<h2>Hi There</h2>')
+})
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
